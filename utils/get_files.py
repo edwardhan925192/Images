c20081142e02bdd8f7df3f711a0c6124c4981fe8
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-28 15:12:40 +0900

    Update get_files.py

--- utils/get_files.py
+++ utils/get_files.py
@@ -1,25 +1,21 @@
 import os
 
-def get_files_list(image_path,mask_path,image_type,mask_type):
+def get_files_list(image_path,image_type):
   '''
   1. Takes path of image 
   2. Takes type of image ex) 'jpg', 'png' 
   3. Returns list of image 
   '''
-  image_dir = image_path
-  mask_dir = mask_path
-
+  image_dir = image_path  
   image_type = image_type
-  mask_type = mask_type
-
+  
   # Check if directories exist and contain files
   if os.path.exists(image_dir) and os.path.exists(mask_dir):
-      image_files = [os.path.join(image_dir, file) for file in os.listdir(image_dir) if file.endswith(f'.{image_type}')]
-      mask_files = [os.path.join(mask_dir, file) for file in os.listdir(mask_dir) if file.endswith(f'.{mask_type}')]
+      image_files = [os.path.join(image_dir, file) for file in os.listdir(image_dir) if file.endswith(f'.{image_type}')]  
 
-      if not image_files or not mask_files:
+      if not image_files :
           print("No files found in the directories")
   else:
       print("One or both directories do not exist")
 
-  return image_files, mask_files
+  return image_files
