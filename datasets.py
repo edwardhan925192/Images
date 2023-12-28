1820b5cb605bef3b731b667198bdc6106648ae4d
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-28 15:00:32 +0900

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,15 +1,14 @@
 from utils.processing_images import preprocess_image
 from torch.utils.data import Dataset
 
-class Image_Mask_Dataset(Dataset):
+class Image_Dataset(Dataset):
     '''
     1. Takes list of image_files
-    2. Preprocess image and mask
-    3. Augment image and mask
+    2. Preprocess image
+    3. Augment image
     '''
-    def __init__(self, image_files, mask_files, input_size=(256, 256), augmentation_transforms=None):
-        self.image_files = image_files
-        self.mask_files = mask_files
+    def __init__(self, image_files, input_size=(224, 224), augmentation_transforms=None):
+        self.image_files = image_files        
         self.input_size = input_size
         self.augmentation_transforms = augmentation_transforms
 
@@ -18,13 +17,11 @@ class Image_Mask_Dataset(Dataset):
 
     def __getitem__(self, idx):
 
-        image_path = self.image_files[idx]
-        mask_path = self.mask_files[idx]
+        image_path = self.image_files[idx]        
 
-        image = preprocess_image(image_path)
-        mask = preprocess_mask(mask_path)
+        image = preprocess_image(image_path)        
 
         if self.augmentation_transforms:
-            image, mask = self.augmentation_transforms(image, mask)
+            image = self.augmentation_transforms(image)
 
-        return image, mask
+        return image
