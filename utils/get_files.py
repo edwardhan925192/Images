7385bdf7850eb0ad78c7bf5977678e2aa02e13f2
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-12-28 15:30:14 +0900

    Update get_files.py

--- utils/get_files.py
+++ utils/get_files.py
@@ -10,7 +10,7 @@ def get_files_list(image_path,image_type):
   image_type = image_type
   
   # Check if directories exist and contain files
-  if os.path.exists(image_dir) and os.path.exists(mask_dir):
+  if os.path.exists(image_dir):
       image_files = [os.path.join(image_dir, file) for file in os.listdir(image_dir) if file.endswith(f'.{image_type}')]  
 
       if not image_files :
