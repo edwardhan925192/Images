0cd1bb72ba8959258fe7577a44efc72a2fb8fc03
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2024-01-01 15:03:39 +0900

    Update ijepa.py

--- models/ijepa.py
+++ models/ijepa.py
@@ -12,11 +12,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from Images.utils.tensors import (
+from utils.tensors import (
     trunc_normal_,
     repeat_interleave_batch
 )
-from Images.utils.masks.mask_application import apply_masks
+from utils.masks.mask_application import apply_masks
 
 
 def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
