d656489d3dca2556e1a52ba6051afee5b0f43601
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2024-01-09 08:34:17 +0900

    Update singlefolder_dataset.py

--- datasets/singlefolder_dataset.py
+++ datasets/singlefolder_dataset.py
@@ -26,22 +26,3 @@ class SingleFolderDataset(Dataset):
 
         return image
 
-# -- transformation
-transform = transforms.Compose([
-    transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
-    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
-    transforms.ToTensor()
-])
-
-# Instantiate the dataset
-dataset = SingleFolderDataset('path/to/your/single/folder', transform)
-
-# DataLoader
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-# Now you can iterate over the DataLoader to get batches of images
-for images in dataloader:
-    # Process the images
-    pass
